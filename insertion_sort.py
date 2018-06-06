a5126dc256f464b5dcdfeedb3e5bcb3c405d047c
nhvantzelfde <nathan.vantzelfde@gmail.com>
2018-06-05 22:23:46 -0400

    Insertion sort implementation

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,20 +1,18 @@
 
-
 def insertionSort(arr):
     """ Sorts array arr in place, using insertion sort
         :type arr: List[object]
         :rtype: none
     """
-    
     for i in range(len(arr)):
         for j in range(i-1,-1,-1):
-            if arr[i] < arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+            if arr[j+1] < arr[j]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
             else:
                 break
 
 def main():
-    arr = [12, 4, 5, 81, 3, 2, 54, 19, 0]
+    arr = [12, 4, 5, 81, 3, 2, 54, 19, 0, 123, 4, 5, 6, 2, -1, 6, 342]
     insertionSort(arr)
     print(arr)
 
