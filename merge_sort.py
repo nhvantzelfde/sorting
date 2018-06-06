51cb2ed9430d535a6df74252ef36e71c744779a8
nhvantzelfde <nathan.vantzelfde@gmail.com>
2018-06-05 22:22:42 -0400

    Standard merge sort implementation

--- merge_sort.py
+++ merge_sort.py
@@ -1,6 +1,19 @@
 
 
-
+def mergeSort(arr):
+    """ Merge sort implementation. Returns sorted array; original array unchanged
+        type arr: List[]
+        rtype: List[]
+    """
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    
+    
+    
+        
+    
 
 
 
